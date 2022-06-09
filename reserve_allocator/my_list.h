c32f5b64e9710c6e6b74ff02119cc8eb96908d54
vadim-ruzh <85361093+vadim-ruzh@users.noreply.github.com>
2022-06-09 12:49:38 +0700

    add my_list class

---
+++ reserve_allocator/my_list.h
@@ -0,0 +1,248 @@
+#pragma once
+#include <stdexcept>
+
+
+namespace my
+{
+	template<typename T, typename Alloc>
+	class List
+	{
+	public:
+		struct Node;
+		class Iterator;
+
+		using nodeAllocator = typename std::allocator_traits<Alloc>::template rebind_alloc<Node>;
+
+		using value_type = typename std::allocator_traits<Alloc>::template value_type;
+		using reference = value_type&;
+		using const_reference = const value_type&;
+		using pointer = typename std::allocator_traits<Alloc>::template pointer;
+		using const_pointer = typename std::allocator_traits<Alloc>::template const_pointer;
+		using size_type = typename std::allocator_traits<Alloc>::template size_type;
+
+
+		struct Node
+		{
+			Node(Node* prev_, Node* next_, const_reference value_) : prev(prev_), next(next_), value(value_) {}
+			
+			Node* prev;
+			Node* next;
+			value_type value;
+		};
+
+		class Iterator
+		{
+		public:
+			Iterator(Node* ptrToNode = nullptr) : mPtr(ptrToNode) {}
+
+			Iterator& operator++()
+			{
+				mPtr = mPtr->next;
+				return *this;
+			}
+
+			Iterator& operator--()
+			{
+				mPtr = mPtr->prev;
+				return *this;
+			}
+
+			reference operator*()
+			{
+				return mPtr->value;
+			}
+
+			bool operator==(const Iterator& other)
+			{
+				return mPtr == other.mPtr;
+			}
+
+			bool operator!=(const Iterator& other)
+			{
+				return mPtr != other.mPtr;
+			}
+
+			Node* mPtr;
+		};
+
+		List() :mHeadNode(nullptr), mSize(0)
+		{
+			mHeadNode = reinterpret_cast<Node*>(std::allocator_traits<nodeAllocator>::allocate(mAllocator, 1));
+
+			mHeadNode->next = mHeadNode;
+			mHeadNode->prev = mHeadNode;
+		}
+
+		//TODO:
+		List(List&& other) noexcept : mAllocator(std::move(other.mAllocator))
+		{
+			mSize = other.mSize;
+			other.mSize = 0;
+
+			mHeadNode = other.mHeadNode;
+			other.mHeadNode = nullptr;
+		}
+
+		//TODO:
+		List(const List& other) noexcept : mHeadNode(other.mHeadNode),mSize(other.mSize)
+		{
+
+		}
+
+		~List()
+		{
+			if (mHeadNode != nullptr)
+			{
+				Node* removableNode = mHeadNode->next;
+
+				while (removableNode != mHeadNode)
+				{
+					auto nextNode = removableNode->next;
+
+					std::allocator_traits<nodeAllocator>::destroy(mAllocator, removableNode);
+					std::allocator_traits<nodeAllocator>::deallocate(mAllocator, removableNode, sizeof(Node));
+
+					removableNode = nextNode;
+				}
+
+				std::allocator_traits<nodeAllocator>::deallocate(mAllocator, removableNode, sizeof(Node));
+			}
+		}
+
+		size_type max_size() const
+		{
+			return std::allocator_traits<nodeAllocator>::max_size(mAllocator)- 1;
+		}
+		size_type size() const
+		{
+			return mSize;
+		}
+		bool empty() const
+		{
+			return !mSize;
+		}
+
+		Iterator insert(Iterator where, const_reference value)
+		{
+			if(mSize + 1 > this->max_size())
+			{
+				throw std::exception("Maximum number of values exceeded");
+			}
+
+			Node* thisNode = where.mPtr;
+
+			Node* newNode = reinterpret_cast<Node*>(std::allocator_traits<nodeAllocator>::allocate(mAllocator, 1));
+
+			std::allocator_traits<nodeAllocator>::construct(mAllocator, newNode, thisNode->prev, thisNode, value);
+
+			thisNode->prev->next = newNode;
+			thisNode->prev = newNode;
+
+			++mSize;
+
+			return newNode;
+		}
+		Iterator insert(Iterator where, size_type count, const_reference value)
+		{
+			while(count > 0)
+			{
+				where = this->insert(where, value);
+				--count;
+			}
+			return where;
+		}
+
+		void push_back(const_reference value)
+		{
+			this->insert(this->end(), value);
+		}
+		void push_front(const_reference value)
+		{
+			this->insert(this->begin(), value);
+		}
+
+		Iterator erase(Iterator where)
+		{
+			Node* thisNode = where.mPtr;
+
+			if (thisNode == mHeadNode)
+			{
+				throw std::exception("you cannot delete a head node");
+			}
+
+			Node* afterThisNode = thisNode->next;
+			Node* beforeThisNode = thisNode->prev;
+
+			afterThisNode->prev = beforeThisNode;
+			beforeThisNode->next = afterThisNode;
+
+			std::allocator_traits<nodeAllocator>::destroy(mAllocator, thisNode);
+			std::allocator_traits<nodeAllocator>::deallocate(mAllocator, thisNode, sizeof(Node));
+
+			--mSize;
+
+			return afterThisNode;
+		}
+		Iterator erase(Iterator begin, Iterator end)
+		{
+			while (begin != end)
+			{
+				begin = this->erase(begin);
+			}
+			
+			return begin;
+		}
+		void pop_back()
+		{
+			Iterator preLastNodeIter = --this->end();
+			this->erase(preLastNodeIter);
+		}
+		void pop_front()
+		{
+			this->erase(this->begin());
+		}
+		void clear()
+		{
+			this->erase(this->begin(), this->end());
+		}
+
+		void remove(const_reference value)
+		{
+			for (auto beg = this->begin(); beg != this->end(); ++beg)
+			{
+				if (*beg == value)
+				{
+					this->erase(beg);
+				}
+			}
+		}
+
+		Iterator begin()
+		{
+			return Iterator(mHeadNode->next);
+		}
+		Iterator end()
+		{
+			return Iterator(mHeadNode);
+		}
+
+		reference back()
+		{
+			return *--this->end();
+		}
+
+		reference front()
+		{
+			return *this->begin();
+		}
+
+		//TODO:
+		//void sort();
+
+	private:
+		nodeAllocator mAllocator;
+		Node* mHeadNode;
+		size_type mSize;
+	};
+
+}
--- reserve_allocator/reserve_allocator.h
+++ reserve_allocator/reserve_allocator.h
@@ -7,142 +7,161 @@
 #include <iostream>
 
 
-static size_t AlignUp(size_t n,size_t Alignment) noexcept
+namespace my
 {
-    if (n % Alignment)
+
+    inline  size_t AlignUp(size_t n, size_t Alignment) noexcept
     {
-        n = (n / Alignment + 1) * Alignment;
-    }
-
-    return n;
-}
-
-template<std::size_t ReserveSize, std::size_t Alignment = alignof(std::max_align_t)>
-class MemoryPool
-{
-public:
-    uint8_t* Allocate(std::size_t requiredMemorySize)
-    {
-	    if (IsNotReserved())
-	    {
-	        ReserveMemory();
-	    }
-
-        const size_t alignMemorySize = AlignUp(requiredMemorySize,Alignment);
-
-        if(IsPointerInMemoryPool(mCurrentPlaceInMemory + alignMemorySize))
+        if (n % Alignment)
         {
-            uint8_t* ptrToAllocatedMemory = mCurrentPlaceInMemory;
-            mCurrentPlaceInMemory += alignMemorySize;
-            mAllocationCounter++;
-
-            return ptrToAllocatedMemory;
+            n = (n / Alignment + 1) * Alignment;
         }
 
-        throw std::runtime_error("Out of bounds of the reserved memory");
+        return n;
     }
 
-    void Deallocate()
+    template<std::size_t ReserveSize, std::size_t Alignment = alignof(std::max_align_t)>
+    class MemoryPool
     {
-    	mAllocationCounter--;
-
-        if(mAllocationCounter == 0)
+    public:
+        uint8_t* Allocate(std::size_t requiredMemorySize)
         {
-            UnReserveMemory();
+            if (IsNotReserved())
+            {
+                ReserveMemory();
+            }
+
+            const size_t alignMemorySize = AlignUp(requiredMemorySize, Alignment);
+
+            if (IsPointerInMemoryPool(mCurrentPlaceInMemory + alignMemorySize))
+            {
+                uint8_t* ptrToAllocatedMemory = mCurrentPlaceInMemory;
+                mCurrentPlaceInMemory += alignMemorySize;
+                mAllocationCounter++;
+
+                return ptrToAllocatedMemory;
+            }
+
+            throw std::runtime_error("Out of bounds of the reserved memory");
         }
-    }
 
-    bool IsPointerInMemoryPool(uint8_t* ptr) const
-    {
-    	return mBeginOfMemory <= ptr && ptr <= mBeginOfMemory + ReserveSize;
-    }
-
-private:
-
-    bool IsNotReserved() const
-    {
-        return mBeginOfMemory == nullptr ? true : false;
-    }
-
-    void ReserveMemory()
-    {
-        if (IsNotReserved())
+        void Deallocate()
         {
-            mBeginOfMemory = static_cast<uint8_t*>(_aligned_malloc(ReserveSize,Alignment));
-            mCurrentPlaceInMemory = mBeginOfMemory;
-        }
-    }
+            mAllocationCounter--;
 
-    void UnReserveMemory()
-    {
-        if(mBeginOfMemory != nullptr)
+            if (mAllocationCounter == 0)
+            {
+                UnReserveMemory();
+            }
+        }
+
+        bool IsPointerInMemoryPool(uint8_t* ptr) const
         {
-            _aligned_free(mBeginOfMemory);
+            return mBeginOfMemory <= ptr && ptr <= mBeginOfMemory + ReserveSize;
         }
-    }
 
-    uint8_t* mBeginOfMemory = nullptr;
-    uint8_t* mCurrentPlaceInMemory = nullptr;
-    std::size_t mAllocationCounter = 0;
-};
+    private:
 
-template<typename T, std::size_t ReservedMemorySize,std::size_t Alignment = alignof(std::max_align_t)>
-class LinearAllocator
-{
-public:
-    using value_type = T;
-    using pointer = T*;
-    using const_pointer = const T*;
-    using reference = T&;
-    using const_reference = const T&;
-    using size_type = size_t;
+        bool IsNotReserved() const
+        {
+            return mBeginOfMemory == nullptr ? true : false;
+        }
 
-    LinearAllocator() : mMemoryPool(std::make_unique<MemoryPool<ReservedMemorySize,Alignment>>()){}
+        void ReserveMemory()
+        {
+            if (IsNotReserved())
+            {
+                mBeginOfMemory = static_cast<uint8_t*>(_aligned_malloc(ReserveSize, Alignment));
+                mCurrentPlaceInMemory = mBeginOfMemory;
+            }
+        }
 
-    template<typename U, size_t UReservedMemorySize, std::size_t UAlignment>
-    LinearAllocator(LinearAllocator<U, UReservedMemorySize, UAlignment> const&) : LinearAllocator(){}
+        void UnReserveMemory()
+        {
+            if (mBeginOfMemory != nullptr)
+            {
+                _aligned_free(mBeginOfMemory);
+            }
+        }
 
-    template<typename U>
-    struct rebind
-    {
-        using other = LinearAllocator<U, ReservedMemorySize,Alignment>;
+        uint8_t* mBeginOfMemory = nullptr;
+        uint8_t* mCurrentPlaceInMemory = nullptr;
+        std::size_t mAllocationCounter = 0;
     };
 
-    static size_type max_size()
+    template<typename T, std::size_t ReservedMemorySize, std::size_t Alignment = alignof(std::max_align_t)>
+    class ReserveAllocator
     {
-        return ReservedMemorySize/AlignUp(sizeof(value_type), Alignment) - 1;
-    }
+    public:
+        using value_type = T;
+        using pointer = T*;
+        using const_pointer = const T*;
+        using reference = T&;
+        using const_reference = const T&;
+        using size_type = size_t;
 
-    pointer allocate(std::size_t size)
-    {
-        const size_t bytesToAllocate = size * sizeof(value_type);
+        ReserveAllocator() : mMemoryPool(std::make_unique<MemoryPool<ReservedMemorySize, Alignment>>()) {}
 
-        return  reinterpret_cast<pointer>(mMemoryPool->Allocate(bytesToAllocate));
-    }
+        template<typename U, size_t UReservedMemorySize, std::size_t UAlignment>
+        ReserveAllocator(ReserveAllocator<U, UReservedMemorySize, UAlignment> const&) : ReserveAllocator() {}
 
-    void deallocate(pointer p, std::size_t n)
-    {
-	    if (mMemoryPool->IsPointerInMemoryPool(reinterpret_cast<uint8_t*>(p)))
-	    {
-            mMemoryPool->Deallocate();
-	    }
-	    else
-	    {
-	        _aligned_free(p);
-	    }
-    }
+        ReserveAllocator(ReserveAllocator&& other) noexcept
+        {
+            mMemoryPool = std::move(other.mMemoryPool);
+            other.mMemoryPool = nullptr;
+        }
 
-    template<typename U, typename ...Args>
-    void construct(U* p, Args &&...args)
-    {
-        new(p) U(std::forward<Args>(args)...);
-    }
+        template<typename U>
+        struct rebind
+        {
+            using other = ReserveAllocator<U, ReservedMemorySize, Alignment>;
+        };
 
-    void destroy(pointer p)
-    {
-        p->~T();
-    }
+        static size_type max_size()
+        {
+            return ReservedMemorySize / AlignUp(sizeof(value_type), Alignment);
+        }
 
-private:
-    std::unique_ptr<MemoryPool<ReservedMemorySize, Alignment>> mMemoryPool;
-};
+        pointer allocate(size_type size)
+        {
+            const size_t bytesToAllocate = size * sizeof(value_type);
+            std::cout << "Allocate  = " << bytesToAllocate << std::endl;
+
+            return  reinterpret_cast<pointer>(mMemoryPool->Allocate(bytesToAllocate));
+        }
+
+        void deallocate(pointer p, size_type n)
+        {
+            std::cout << "deallocate " << std::endl;
+
+            if (mMemoryPool->IsPointerInMemoryPool(reinterpret_cast<uint8_t*>(p)))
+            {
+                mMemoryPool->Deallocate();
+            }
+            else
+            {
+                _aligned_free(p);
+            }
+        }
+
+        template<typename U, typename ...Args>
+        void construct(U* p, Args &&...args)
+        {
+            std::cout << "construct" << std::endl;
+
+            new(p) U(std::forward<Args>(args)...);
+        }
+
+        void destroy(pointer p)
+        {
+            std::cout << "destroy " << std::endl;
+
+            p->~T();
+        }
+
+    private:
+        std::unique_ptr<MemoryPool<ReservedMemorySize, Alignment>> mMemoryPool;
+    };
+
+
+}
